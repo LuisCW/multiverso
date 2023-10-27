b589be8317a4eff0f40c1efa6217454e7c614f09
LuisCW <92056981+LuisCW@users.noreply.github.com>
2023-10-27 08:37:30 -0500

    Add files via upload

---
+++ niveles/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+
+def mi_multiverso(request):
+    # Definir la estructura del multiverso como un grafo
+    grafo_multiverso = {
+        1: [2],     # Nivel 1 está conectado al Nivel 2
+        2: [3, 4],  # Nivel 2 está conectado a los Niveles 3 y 4
+        3: [5],     # Nivel 3 está conectado al Nivel 5
+        4: [5, 6],  # Nivel 4 está conectado a los Niveles 5 y 6
+        # Agrega más conexiones según sea necesario
+    }
+
+    # Obtener el nivel actual del usuario (por ejemplo, nivel 3)
+    nivel_actual = 3  # Puedes cambiar esto según tus necesidades o obtenerlo de la base de datos
+
+    # Obtener los niveles desbloqueados hasta el nivel actual
+    niveles_desbloqueados = set()
+    cola = [1]  # Comienza desde el primer nivel
+    while cola:
+        nivel = cola.pop(0)
+        if nivel <= nivel_actual:
+            niveles_desbloqueados.add(nivel)
+            cola.extend(grafo_multiverso.get(nivel, []))
+
+    context = {
+        'niveles': range(1, 31),
+    }
+
+    return render(request, 'nivel.html', context)
+
+
