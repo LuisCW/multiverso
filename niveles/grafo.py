979cf6488a25e37d9d0d12d3973be83154aaedc6
Ivan Silva <isilvao@unal.edu.co>
2023-11-07 02:05:46 -0500

    Finalizacion del grafo dirigido para pasar entre niveles o mundos.
    Hacen falta los html de cada mundo y ya está listo :D

--- niveles/grafo.py
+++ niveles/grafo.py
@@ -1,13 +1,20 @@
-class Node:
-    def __init__(self, html):
-        self.data = html
-        self.left = None
-        self.right = None
+class Graph:
+    def __init__(self):
+        self.graph = {}
 
-    def addNode(self, html):
-        if self.left is None:
-            self.left = Node(html)
-        elif self.right is None:
-            self.right = Node(html)
+    def add_node(self, node):
+        if node not in self.graph:
+            self.graph[node] = []
+
+    def add_edge(self, node1, node2):
+        if node1 in self.graph and node2 in self.graph:
+            self.graph[node1].append(node2)
+
+    def get_neighbors(self, node):
+        if node in self.graph:
+            return self.graph[node]
         else:
-            self.left.addNode(html)
+            return []
+
+    def __str__(self):
+        return str(self.graph)
--- niveles/views.py
+++ niveles/views.py
@@ -1,38 +1,96 @@
 from django.shortcuts import render
-from .grafo import Node
+from .grafo import Graph
 
 
 def niveles(request):
     if request.method == "POST":
         if "boton_izquierda" in request.POST:
-            mundo = siguienteMundo(1)
+            mundo = str(cambiarNodo(1))
         elif "boton_derecha" in request.POST:
-            mundo = siguienteMundo(2)
+            mundo = str(cambiarNodo(2))
 
-        return render(request, mundo)
+        return render(request, "nivel" + mundo + ".html")
 
     else:
         # Creacion del grafo
-        root = Node("nivel1.html")
+        global graph
+        graph = Graph()
 
         # Asignacion de los nodos
-        for i in range(2, 37):
-            root.addNode("nivel" + str(i) + ".html")
+        for i in range(1, 37):
+            graph.add_node(i)
 
-        global nodo
-        nodo = root
+        # Asignacion de los arcos
+        graph.add_edge(1, 2)
+        graph.add_edge(1, 3)
+        graph.add_edge(2, 4)
+        graph.add_edge(2, 5)
+        graph.add_edge(3, 6)
+        graph.add_edge(3, 7)
+        graph.add_edge(4, 8)
+        graph.add_edge(4, 9)
+        graph.add_edge(5, 10)
+        graph.add_edge(5, 11)
+        graph.add_edge(6, 12)
+        graph.add_edge(6, 13)
+        graph.add_edge(7, 14)
+        graph.add_edge(7, 15)
+        graph.add_edge(8, 16)
+        graph.add_edge(8, 17)
+        graph.add_edge(9, 18)
+        graph.add_edge(9, 19)
+        graph.add_edge(10, 20)
+        graph.add_edge(10, 21)
+        graph.add_edge(11, 22)
+        graph.add_edge(11, 23)
+        graph.add_edge(12, 24)
+        graph.add_edge(12, 25)
+        graph.add_edge(13, 26)
+        graph.add_edge(13, 27)
+        graph.add_edge(14, 28)
+        graph.add_edge(14, 29)
+        graph.add_edge(15, 30)
+        graph.add_edge(15, 31)
+        graph.add_edge(16, 32)
+        graph.add_edge(16, 33)
+        graph.add_edge(31, 34)
+        graph.add_edge(31, 35)
+        graph.add_edge(17, 36)
+        graph.add_edge(18, 36)
+        graph.add_edge(19, 36)
+        graph.add_edge(20, 36)
+        graph.add_edge(21, 36)
+        graph.add_edge(22, 36)
+        graph.add_edge(23, 36)
+        graph.add_edge(24, 36)
+        graph.add_edge(25, 36)
+        graph.add_edge(26, 36)
+        graph.add_edge(27, 36)
+        graph.add_edge(28, 36)
+        graph.add_edge(29, 36)
+        graph.add_edge(30, 36)
+        graph.add_edge(32, 36)
+        graph.add_edge(33, 36)
+        graph.add_edge(34, 36)
+        graph.add_edge(35, 36)
+
+        global nodo_actual
+
+        nodo_actual = 1
 
         return render(request, "nivel1.html")
 
 
-def siguienteMundo(mundo):
-    global nodo
+def cambiarNodo(nodo):
+    global nodo_actual, graph
+    if (nodo_actual >= 17 and nodo_actual <= 35) and nodo_actual != 31:
+        nodo_actual = 36
+    elif nodo_actual == 36:
+        nodo_actual = 0
+    else:
+        if nodo == 1:
+            nodo_actual = graph.get_neighbors(nodo_actual)[0]
+        elif nodo == 2:
+            nodo_actual = graph.get_neighbors(nodo_actual)[1]
 
-    if mundo == 1:
-        nodo = nodo.left
-        print("Este es el mundo: ", nodo.data)
-        return nodo.data
-    elif mundo == 2:
-        nodo = nodo.right
-        print("Este es el mundo: ", nodo.data)
-        return nodo.data
+    return nodo_actual
